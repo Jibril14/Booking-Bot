841beabd2966d7790c17a4fd0cf71a7cfaf2bd44
Abdullahi Abdulwasiu <47268671+Jibril14@users.noreply.github.com>
2022-02-10 18:10:00 +0100

    Close Browser Functionality

--- booking/booking.py
+++ booking/booking.py
@@ -6,10 +6,11 @@ from selenium.webdriver.chrome.options import Options
 
 # each instance inherit from webdriver.Chrome
 class Booking(webdriver.Chrome):
-    def __init__(self, driver_path=r"C:\Chromedriver\chromedriver"):
+    def __init__(self, driver_path=r"C:\Chromedriver\chromedriver", tear_down=False):
         self.driver_path = driver_path
         os.environ['PATH'] += self.driver_path
         # enable chromedriver keep browser context
+        self.tear_down = tear_down
         chrome_options = Options()
         chrome_options.add_experimental_option("detach", True)
         # instanciate webdriver.Chrome along & pass in chrome option
@@ -17,5 +18,10 @@ class Booking(webdriver.Chrome):
         self.implicitly_wait(15)
         self.maximize_window()
 
+    # exit the web browser on tear down
+    def __exit__(self, exe_type, exc_val, exc_tb):
+        if self.tear_down:
+            self.quit()
+            
     def land_first_page(self):
         self.get(const.BASE_URL)
--- main.py
+++ main.py
@@ -2,7 +2,9 @@ from booking.booking import Booking
 
 
 
-
-obj = Booking()
-obj.land_first_page()
+with Booking() as bot:
+    bot.land_first_page()
+    print("Exiting!")
+#obj = Booking()
+#obj.land_first_page()
 #obj.get("https://web.whatsapp.com")
